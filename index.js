c3e4bedc6ba0897198fb7121aede8c33ddab8b7d
bykadorovatanya1991-maker <bykadorova.tanya1991@gmail.com>
2026-07-14 11:42:29 +0200

    style

--- index.js
+++ index.js
@@ -1,72 +1,80 @@
 function gewinn(winner) {
-  alert(winner + " hat gewonnen!");
+  document.getElementById("winnerMessage").textContent =
+    winner + " hat gewonnen!";
+  isRunning = false 
 
   document.getElementById("restartButton").style.display = "block";
 }
 
 function checkGewinn (playfield) {
-if (playfield[0][0].textContent === playfield[0][1].textContent && playfield[0][1].textContent === playfield[0][2].textContent
- &&
- playfield[0][0].textContent.trim() !== ``) {
- gewinn (playfield[0][0].textContent)
- }
-}
+  if (playfield[0][0].textContent === playfield[0][1].textContent && playfield[0][1].textContent === playfield[0][2].textContent
+    &&
+    playfield[0][0].textContent.trim() !== ``) {
+    gewinn (playfield[0][0].textContent)
+    }
 
-function checkGewinn (playfield) {
- if (playfield[1][0].textContent === playfield[1][1].textContent && playfield[1][1].textContent === playfield[1][2].textContent
- &&
- playfield[1][0].textContent.trim() !== ``) {
- gewinn (playfield[1][0].textContent)
-}
+  if (playfield[1][0].textContent === playfield[1][1].textContent && playfield[1][1].textContent === playfield[1][2].textContent
+    &&
+    playfield[1][0].textContent.trim() !== ``) {
+    gewinn (playfield[1][0].textContent)
+    }
 
+  if (playfield[2][0].textContent === playfield[2][1].textContent && playfield[2][1].textContent === playfield[2][2].textContent
+    &&
+    playfield[2][0].textContent.trim() !== ``) {
+    gewinn (playfield[2][0].textContent)
+    }
 
-if (playfield[2][0].textContent === playfield[2][1].textContent && playfield[2][1].textContent === playfield[2][2].textContent
- &&
- playfield[2][0].textContent.trim() !== ``) {
- gewinn (playfield[2][0].textContent)
-}
+  if (playfield[0][0].textContent === playfield[1][0].textContent && playfield[1][0].textContent === playfield[2][0].textContent
+    &&
+    playfield[0][0].textContent.trim() !== ``) {
+    gewinn (playfield[0][0].textContent)
+  }
 
-if (playfield[0][0].textContent === playfield[1][0].textContent && playfield[1][0].textContent === playfield[2][0].textContent
- &&
- playfield[0][0].textContent.trim() !== ``) {
- gewinn (playfield[0][0].textContent)
-}
+  if (playfield[0][1].textContent === playfield[1][1].textContent && playfield[1][1].textContent === playfield[2][1].textContent
+    &&
+    playfield[0][1].textContent.trim() !== ``) {
+    gewinn (playfield[0][1].textContent)
+  }
 
-if (playfield[0][1].textContent === playfield[1][1].textContent && playfield[1][1].textContent === playfield[2][1].textContent
- &&
- playfield[0][1].textContent.trim() !== ``) {
- gewinn (playfield[0][1].textContent)
-}
+  if (playfield[0][2].textContent === playfield[1][2].textContent && playfield[1][2].textContent === playfield[2][2].textContent
+    &&
+    playfield[0][2].textContent.trim() !== ``) {
+    gewinn (playfield[0][2].textContent)
+  }
 
-if (playfield[0][2].textContent === playfield[1][2].textContent && playfield[1][2].textContent === playfield[2][2].textContent
- &&
- playfield[0][2].textContent.trim() !== ``) {
- gewinn (playfield[0][2].textContent)
-}
+  if (playfield[0][0].textContent === playfield[1][1].textContent && playfield[1][1].textContent === playfield[2][2].textContent
+    &&
+    playfield[0][0].textContent.trim() !== ``) {
+    gewinn (playfield[0][0].textContent)
+  }
 
-if (playfield[0][0].textContent === playfield[1][1].textContent && playfield[1][1].textContent === playfield[2][2].textContent
- &&
- playfield[0][0].textContent.trim() !== ``) {
- gewinn (playfield[0][0].textContent)
-}
-
-if (playfield[2][0].textContent === playfield[1][1].textContent && playfield[1][1].textContent === playfield[0][2].textContent
- &&
- playfield[2][0].textContent.trim() !== ``) {
- gewinn (playfield[2][0].textContent)
-}
+  if (playfield[2][0].textContent === playfield[1][1].textContent && playfield[1][1].textContent === playfield[0][2].textContent
+    &&
+    playfield[2][0].textContent.trim() !== ``) {
+    gewinn (playfield[2][0].textContent)
+  }
 }
 
 
 function myFunc(button) {
+if (isRunning === false) {return}
   if (button.textContent.trim() === "") {
     button.textContent = player;
-    if(player ===  "X") {
-      player = "O"
+    checkGewinn(playfield);
+    if (player === "X") {
+      player = "O";
     } else {
-      player = "X"
+      player = "X";
     }
+    document.getElementById("currentPlayer").textContent = player;
   }
+}
+
+let player = "X"
+let playfield = [[],[],[]]
+let isRunning = true
+
 
   for(let j = 0; j < 3; j++) {
     for(let i = 0; i < 3; i++) {
@@ -78,17 +86,15 @@ function myFunc(button) {
     }
   }
 
-  checkGewinn(playfield)
-
- 
-}
 function restartGame() {
   for (let j = 0; j < 3; j++) {
-  for (let i = 0; i < 3; i++) {playfield[j][i].textContent = "";}
+    for (let i = 0; i < 3; i++) {
+      playfield[j][i].textContent = "";
+    }
   }
-player = "X";
-document.getElementById("restartButton").style.display = "none";
+  document.getElementById("winnerMessage").textContent = "";
+  player = "X";
+  isRunning = true;
+  document.getElementById("currentPlayer").textContent = "X";
+  document.getElementById("restartButton").style.display = "none";
 }
-
-let player = "X"
-let playfield = [[],[],[]]
