5e93fc63d24cdb7b9375662a6cf877af96c56bb8
bykadorovatanya1991-maker <bykadorova.tanya1991@gmail.com>
2026-07-14 09:06:08 +0200

    refactor

--- index.js
+++ index.js
@@ -1,74 +1,151 @@
+function gewinn (winner) {
+  alert(winner + " hat gewonnen!")
+}
+
+function checkGewinn (playfield) {
+if (playfield[0][0].textContent === playfield[0][1].textContent && playfield[0][1].textContent === playfield[0][2].textContent
+&&
+playfield[0][0].textContent.trim() !== ``) {
+gewinn (playfield[0][0].textContent)
+}
+}
+
+function checkGewinn (playfield) {
+if (playfield[1][0].textContent === playfield[1][1].textContent && playfield[1][1].textContent === playfield[1][2].textContent
+&&
+playfield[1][0].textContent.trim() !== ``) {
+gewinn (playfield[1][0].textContent)
+}
+
+
+if (playfield[2][0].textContent === playfield[2][1].textContent && playfield[2][1].textContent === playfield[2][2].textContent
+&&
+playfield[2][0].textContent.trim() !== ``) {
+gewinn (playfield[2][0].textContent)
+}
+
+if (playfield[0][0].textContent === playfield[1][0].textContent && playfield[1][0].textContent === playfield[2][0].textContent
+&&
+playfield[0][0].textContent.trim() !== ``) {
+gewinn (playfield[0][0].textContent)
+}
+
+if (playfield[0][1].textContent === playfield[1][1].textContent && playfield[1][1].textContent === playfield[2][1].textContent
+&&
+playfield[0][1].textContent.trim() !== ``) {
+gewinn (playfield[0][1].textContent)
+}
+
+if (playfield[0][2].textContent === playfield[1][2].textContent && playfield[1][2].textContent === playfield[2][2].textContent
+&&
+playfield[0][2].textContent.trim() !== ``) {
+gewinn (playfield[0][2].textContent)
+}
+
+if (playfield[0][0].textContent === playfield[1][1].textContent && playfield[1][1].textContent === playfield[2][2].textContent
+&&
+playfield[0][0].textContent.trim() !== ``) {
+gewinn (playfield[0][0].textContent)
+}
+
+if (playfield[2][0].textContent === playfield[1][1].textContent && playfield[1][1].textContent === playfield[0][2].textContent
+&&
+playfield[2][0].textContent.trim() !== ``) {
+gewinn (playfield[2][0].textContent)
+}
+}
+
+
 function myFunc(button) {
   if (button.textContent.trim() === "") {
 button.textContent = player;
 if(player ===  "X") {player = "O"
  } else {
   player = "X"
- }
-
- const button1 = document.getElementById ("meinButton1");
-const button2 = document.getElementById ("meinButton2");
-const button3 = document.getElementById ("meinButton3");
-if (button1.textContent === button2.textContent && 
-button2.textContent === button3.textContent && 
-button1.textContent.trim() !== ``) {
-  alert(button1.textContent + " hat gewonnen!")
+}
 }
 
-const button4 = document.getElementById ("meinButton4");
-const button5 = document.getElementById ("meinButton5");
-const button6 = document.getElementById ("meinButton6");
-if (button4.textContent === button5.textContent && 
-button5.textContent === button6.textContent && 
-button4.textContent.trim() !== ``) {
-  alert(button4.textContent + " hat gewonnen!")
+let playfield = [[],[],[]]
+
+for(let j = 0; j < 3; j++) {
+  for(let i = 0; i < 3; i++) {
+    try {
+      playfield[j][i] = document.getElementById(`meinButton${j*3+i+1}`)  
+    } catch(e) {
+      alert(e)
+    }
+  }
 }
 
-const button7 = document.getElementById ("meinButton7");
-const button8 = document.getElementById ("meinButton8");
-const button9 = document.getElementById ("meinButton9");
-if (button7.textContent === button8.textContent && 
-button8.textContent === button9.textContent && 
-button7.textContent.trim() !== ``) {
-  alert(button7.textContent + " hat gewonnen!")
-}
+checkGewinn(playfield)
 
 
-if (button1.textContent === button4.textContent && 
-button4.textContent === button7.textContent && 
-button1.textContent.trim() !== ``) {
-  alert(button1.textContent + " hat gewonnen!")
-}
+
+// j=0 i=0 -> button1
+// j=0 i=1 -> button2
 
 
-if (button2.textContent === button5.textContent && 
-button5.textContent === button8.textContent && 
-button2.textContent.trim() !== ``) {
-  alert(button2.textContent + " hat gewonnen!")
-}
 
 
-if (button3.textContent === button6.textContent && 
-button6.textContent === button9.textContent && 
-button3.textContent.trim() !== ``) {
-  alert(button3.textContent + " hat gewonnen!")
-}
+// if (playfield[0][0] === playfield[0][1]=== playfield[0][2] 
+// &&
+// playfield[0][0].trim() !== ``) {
+// }
+
+// if (playfield[0][0] === playfield[0][1]=== playfield[0][2] 
+// &&
+// playfield[0][0].trim() !== ``) {
+// }
+
+// if (button4.textContent === button5.textContent && 
+// button5.textContent === button6.textContent && 
+// button4.textContent.trim() !== ``) {
+//   alert(button4.textContent + " hat gewonnen!")
+// }
+
+// if (button7.textContent === button8.textContent && 
+// button8.textContent === button9.textContent && 
+// button7.textContent.trim() !== ``) {
+//   alert(button7.textContent + " hat gewonnen!")
+// }
 
 
-if (button1.textContent === button5.textContent && 
-button5.textContent === button9.textContent && 
-button1.textContent.trim() !== ``) {
-  alert(button1.textContent + " hat gewonnen!")
-}
+// if (button1.textContent === button4.textContent && 
+// button4.textContent === button7.textContent && 
+// button1.textContent.trim() !== ``) {
+//   alert(button1.textContent + " hat gewonnen!")
+// }
 
 
-if (button3.textContent === button5.textContent && 
-button5.textContent === button7.textContent && 
-button3.textContent.trim() !== ``) {
-  alert(button3.textContent + " hat gewonnen!")
-}
+// if (button2.textContent === button5.textContent && 
+// button5.textContent === button8.textContent && 
+// button2.textContent.trim() !== ``) {
+//   alert(button2.textContent + " hat gewonnen!")
+// }
 
-}
+
+// if (button3.textContent === button6.textContent && 
+// button6.textContent === button9.textContent && 
+// button3.textContent.trim() !== ``) {
+//   alert(button3.textContent + " hat gewonnen!")
+// }
+
+
+// if (button1.textContent === button5.textContent && 
+// button5.textContent === button9.textContent && 
+// button1.textContent.trim() !== ``) {
+//   alert(button1.textContent + " hat gewonnen!")
+// }
+
+
+// if (button3.textContent === button5.textContent && 
+// button5.textContent === button7.textContent && 
+// button3.textContent.trim() !== ``) {
+//   alert(button3.textContent + " hat gewonnen!")
+// }
+
+// }
+// }
 }
 
 let player = "X"
